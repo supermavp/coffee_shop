ced95a32d53260533bfc39bbe643f89bc70d5b8c
unknown <Michael A. Vega Pabon>
2025-01-02 23:25:41 -0500

    feat: Completing list and create product.

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductFormView
+from .views import ProductListAPI, ProductFormView, ProductListView
 
 urlpatterns = [
-    path('add/', ProductFormView.as_view(), name='add_product')
+    path('api/', ProductListAPI.as_view(), name='list_product_api'),
+    path('', ProductListView.as_view(), name='list_product'),
+    path('add/', ProductFormView.as_view(), name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,32 @@
-from django.views import generic
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from django.views import generic
+from django.urls import reverse_lazy
 from products.forms import ProductForm
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 class ProductFormView(generic.FormView):
     template_name = 'products/add_product.html'
     form_class = ProductForm
+    success_url = reverse_lazy('add_product')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
+
+class ProductListAPI(APIView):
+    autentication_clases = []
+    permission_clases = []
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
